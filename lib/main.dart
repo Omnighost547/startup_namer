c1d5dfbb95f564da7c92f0c396c764427687e22e
Tommy Kearns <tommyamkearns@gmail.com>
2019-07-09 23:06:42 +0100

    Step 5: Add a Stateful Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';  // Add this line.
 
-void main() => runApp(MyApp());
+void main() => runApp(MyApp()); // Lambda!
+
+/* // Could alternatively written main() this way:
+void main() {
+  runApp(MyApp());
+}
+*/
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random(); // Add this line.
+    //final wordPair = WordPair.random(); // Added in Step 3, removed in Step 4
+
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
@@ -15,10 +22,24 @@ class MyApp extends StatelessWidget {
           title: const Text('Welcome to Flutter'),
         ),
         body: Center(
-          //child: const Text('Hello World'),   // Replace this text...
-          child: Text(wordPair.asPascalCase),  // With this text.
+          //child: const Text('Hello World'),   // Replaced in Step 4
+          //child: Text(wordPair.asPascalCase),  // Added in Step 4, moved to RandomWordsState in Step 5
+          child: RandomWords(),
         ),
       ),
     );
   }
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final WordPair wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
 }
