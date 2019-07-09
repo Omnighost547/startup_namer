83f475a6bf1265b2226b5ae1d95c6456bdea1a72
Tommy Kearns <tommyamkearns@gmail.com>
2019-07-09 22:32:49 +0100

    Step 4: Use an External Package

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';  // Add this line.
 
 void main() => runApp(MyApp());
 
@@ -6,14 +7,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random(); // Add this line.
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
         ),
-        body: const Center(
-          child: const Text('Hello World'),
+        body: Center(
+          //child: const Text('Hello World'),   // Replace this text...
+          child: Text(wordPair.asPascalCase),  // With this text.
         ),
       ),
     );
